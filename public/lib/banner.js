57b8fd1be7236d6607d3671e9570b49ac201bedf
Rachouan Rejeb <info@rachouan.com>
2023-12-19 15:50:34 +0100

    Adds color modes

--- public/lib/banner.js
+++ public/lib/banner.js
@@ -1,18 +1,33 @@
-const PBanner = {
-  init: (element, options) => {
-    const banner = document.querySelector(element);
+class PBanner {
+  constructor() {
+    this.element = "#banner";
+    this.options = {
+      theme: "system",
+      fixed: true,
+    };
+  }
+  init(element, options) {
+    this.element = element;
+    this.options = { ...this.options, ...options };
+    document.head.innerHTML +=
+      '<link rel="stylesheet" href="/lib/banner-style.css" type="text/css"/>';
+  }
+  create() {
+    const banner = document.querySelector(this.element);
+    const { theme, fixed } = this.options;
 
-    if (banner) {
+    if (!banner) {
+      throw new Error("You must provide an element to create the banner in");
+    } else {
       banner.innerHTML = `
-        <div class="banner" style="display: flex, height: 48, gap: 12; background: #ff0000">
-          <p class="banner__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, quia.</p>
-          <a href="#" class="banner__link">Click here</a>
+        <div class="fp__banner ${theme} ${fixed && "fixed"}">
+          <p class="fp__banner__description">Isreals genocide against 🇵🇸 Palestine needs to stop. Our hears are with those who suffer</p>
+          <a href="#" class="fp__banner__link" href="https://ceasefiretoday.com/" target="_blank">🍉 Palestine</a>
         </div>
       `;
+      console.log("🇵🇸 Free Palestine Banner init with options", this.options);
     }
+  }
+}
 
-    console.log("🇵🇸 Free Palestine Banner init with options", options);
-  },
-};
-
-window.PBanner = PBanner;
+window.PBanner = new PBanner();
